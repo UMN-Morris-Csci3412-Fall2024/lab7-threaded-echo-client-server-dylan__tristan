60e7d84ba261f7d061e832ad58f5ac3fd95f1ba1
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-10-31 11:06:22 -0500

    Created EchoServer framework
    Co-authored-by: Dylan Packer <packe089@users.noreply.github.com>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,9 +7,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
+
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013;
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
@@ -19,13 +20,39 @@ public class EchoServer {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 		while (true) {
 			Socket socket = serverSocket.accept();
-
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
+			ClientHandler clientHandler = new ClientHandler(socket);
+			Thread thread = new Thread(clientHandler);
+			// https://stackoverflow.com/questions/10092251/creating-dynamic-number-of-threads-concurrently
+			thread.start();
 		}
+	}
+
+	private static class ClientHandler implements Runnable {
+		private final Socket socket;
+
+		public ClientHandler(Socket socket) {
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try (InputStream input = socket.getInputStream();
+				OutputStream output = socket.getOutputStream()) {
+				byte[] buffer = new byte[1024]; // added a buffer and commented out the flush
+				int nextByte;
+				while ((nextByte = input.read(buffer)) != -1) {
+					output.write(buffer, 0, nextByte);
+				}
+				//output.flush();
+				 } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 	}
 }
