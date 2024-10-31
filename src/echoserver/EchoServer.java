694c71f8d52340838d151b03b43939e58436d156
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-10-31 11:17:20 -0500

    Added multi-threading to EchoServer using threadpooling
    Co-authored-by: Dylan Packer <packe089@users.noreply.github.com>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -5,11 +5,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class EchoServer {
 
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
 	public static final int PORT_NUMBER = 6013;
+	public static final int THREADPOOL_SIZE = 10;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
@@ -18,12 +21,15 @@ public class EchoServer {
 
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+		ExecutorService threadpool = Executors.newFixedThreadPool(THREADPOOL_SIZE);
 		while (true) {
 			Socket socket = serverSocket.accept();
+			//ExecutorService threadpool = Executors.newFixedThreadPool(THREADPOOL_SIZE);
+
 			ClientHandler clientHandler = new ClientHandler(socket);
-			Thread thread = new Thread(clientHandler);
-			// https://stackoverflow.com/questions/10092251/creating-dynamic-number-of-threads-concurrently
-			thread.start();
+			//Thread thread = new Thread(clientHandler);
+			//thread.start();
+			threadpool.submit(clientHandler);
 		}
 	}
 
